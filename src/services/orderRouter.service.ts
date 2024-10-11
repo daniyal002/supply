7c5bd4b47b2a0ea8b02eac7af30bb25aaae32bf8
daniyal002 <daniyalou@gmail.com>
2024-10-11 17:33:41 +0300

    Formatted code in orderRouter.service.ts, added input validation to getOrderRouteById method

--- src/services/orderRouter.service.ts
+++ src/services/orderRouter.service.ts
@@ -1,18 +1,32 @@
 import { axiosWidthAuth } from "@/api/interseptors";
-import { IAddRouterRequest, IOrderRouteByIdResponse, IOrderRouteResponse, IOrderRouteResponseDetail } from "@/interface/orderRoute";
+import {
+  IAddRouterRequest,
+  IOrderRouteByIdResponse,
+  IOrderRouteResponse,
+  IOrderRouteResponseDetail,
+} from "@/interface/orderRoute";
 
 export const orderRouteService = {
-    async getOrderRoute(){
-        const response = await axiosWidthAuth.get<IOrderRouteResponse>('route/get_route');
-        return response.data;
-    },
+  async getOrderRoute() {
+    const response = await axiosWidthAuth.get<IOrderRouteResponse>(
+      "route/get_route"
+    );
+    return response.data;
+  },
 
-    async getOrderRouteById(routeId:number){
-        const response = await axiosWidthAuth.get<IOrderRouteByIdResponse>(`route/get_route_by_id?route_id=${routeId}`);
-        return response.data.detail;
-    },
-    async addOrderRoute(data:IAddRouterRequest) {
-        const response = await axiosWidthAuth.post<IOrderRouteResponseDetail>("route/add_route",data)
-        return response.data
+  async getOrderRouteById(routeId: number) {
+    if (!isNaN(Number(routeId)) && Number(routeId) > 0) {
+      const response = await axiosWidthAuth.get<IOrderRouteByIdResponse>(
+        `route/get_route_by_id?route_id=${routeId}`
+      );
+      return response.data.detail;
     }
-}
+  },
+  async addOrderRoute(data: IAddRouterRequest) {
+    const response = await axiosWidthAuth.post<IOrderRouteResponseDetail>(
+      "route/add_route",
+      data
+    );
+    return response.data;
+  },
+};
