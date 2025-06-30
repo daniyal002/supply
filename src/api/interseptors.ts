9abc34c063f844dbdeddf8d6f8b65a410b8db2e0
daniyal002 <daniyalou@gmail.com>
2025-06-30 09:19:57 +0300

    added in interseptors baseURL prodaction and develop mode

--- src/api/interseptors.ts
+++ src/api/interseptors.ts
@@ -10,7 +10,7 @@ import { message } from "antd";
 import axios, { type CreateAxiosDefaults } from "axios";
 
 const options: CreateAxiosDefaults = {
-  baseURL: "http://192.168.30.153:8000",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://192.168.30.153:8000",
   headers: {
     "Content-Type": "application/json",
   },
