efb384870fadff76ff4203942b3e39141feb64c3
daniyal002 <daniyalou@gmail.com>
2025-07-03 09:54:16 +0300

    fixed in approvaOrder status_id

--- src/app/approval/[slug]/ApprovalOrder.tsx
+++ src/app/approval/[slug]/ApprovalOrder.tsx
@@ -253,7 +253,7 @@ export default function ApprovalOrder({
       },
       oms: getOrderByIdData?.oms,
       order_route_id: 1,
-      order_status_id: getOrderByIdData?.order_status?.order_status_id,
+      order_status_id: getOrderByIdData?.order_status?.status_id,
       note: getOrderByIdData?.note,
       order_products: getOrderByIdData?.order_products,
       order_type:{
